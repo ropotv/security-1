ece474588c58f2a9d2255ecb17b26dad7d5b5db1
Vasile Ropot <vasile.ropot@codefactorygroup.com>
2020-10-06 12:48:05 +0300

    add Password Encryption

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,24 +1,33 @@
-package com.ropot;
+package app;
+
+import core.utils.PasswordUtils;
 
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        String password = "password123";
         String input = "";
+        String myPassword = "myPassword123";
+
+        String salt = PasswordUtils.getSalt(30);
+        String password = PasswordUtils.generateSecurePassword(myPassword, salt);
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hello! Enter your password to continue.");
+
         while (true) {
             input = scanner.next();
+            boolean passwordMatch = PasswordUtils.verifyUserPassword(input, password, salt);
 
-            if (input.equals(password)) {
+            if (passwordMatch) {
                 System.out.println("Welcome to the application!");
                 break;
             } else {
                 System.out.println("Wrong password! Try again.");
             }
         }
+
     }
 }
